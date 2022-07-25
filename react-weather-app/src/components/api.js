f1e25bb2307907c6be1bbfa4000d00f8fac64311
ankitchy123 <ankitchaudhary.ac9611@gmail.com>
2022-07-25 23:36:19 +0530

    Search

---
+++ react-weather-app/src/components/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'b647e4ab5amsh31fcc9cef146d74p1eee82jsn55f50b7228b7',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
