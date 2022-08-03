3fd090cf29696d0e5d82ac84c85457fd9cd53cb0
ankitchy123 <ankitchaudhary.ac9611@gmail.com>
2022-08-03 12:33:31 +0530

    bug fixed

--- react-weather-app/src/components/api.js
+++ react-weather-app/src/components/api.js
@@ -6,8 +6,8 @@ export const geoApiOptions = {
     }
 };
 
-export const GEO_API_URL = process.env.REACT_APP_GEO_API_URL
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
-export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 
 export const WEATHER_API_KEY = "45e0cfea56b94071e2993fbdf3d6998a"
